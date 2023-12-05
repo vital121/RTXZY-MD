29083e75f3bae82b931e416823405fb5db7283a7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-05 22:32:27 +0700

    Update tools-uploader.js

--- plugins/tools-uploader.js
+++ plugins/tools-uploader.js
@@ -1,3 +1,4 @@
+const uploadFile = require('../lib/uploadFile')
 const uploadImage = require('../lib/uploadImage')
 
 let handler = async (m) => {
@@ -6,18 +7,17 @@ let handler = async (m) => {
   if (!mime) throw 'Tidak ada media yang ditemukan'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  
-  let fileSizeLimit = 5 * 1024 * 1024 // 5MB 🗿
+  let fileSizeLimit = 5 * 1024 * 1024 
   if (media.length > fileSizeLimit) {
     throw 'Ukuran media tidak boleh melebihi 5MB'
   }
-  
-  let link = await (isTele ? uploadImage : uploadImage)(media)
-  m.reply(`${link}\n${media.length} Byte(s)`);
+  let link = await (isTele ? uploadImage : uploadFile)(media)
+  m.reply(`${link}
+${media.length} Byte(s)
+${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
 }
-
 handler.help = ['tourl <reply image>']
-handler.tags = ['tools']
+handler.tags = ['sticker']
 handler.command = /^(upload|tourl)$/i
 
 module.exports = handler
